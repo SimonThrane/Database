68eb4400a2f0610491828438c4740d7f5c1c9650
SimonThrane <minijordan23-virum@hotmail.com>
2016-09-30 09:41:25 +0200

    Opdatering af DAL PersonDataUtil

--- Hand2in/DataAccesTier/PersonUtil.cs
+++ Hand2in/DataAccesTier/PersonUtil.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +10,6 @@ namespace DataAccesTier
 {
     public class PersonDataUtil
     {
-        private Person locPerson;
-
         public PersonDataUtil()
         {
             
@@ -55,96 +54,94 @@ namespace DataAccesTier
             // prepare command string
             string updateString =
                @"UPDATE Person
-                        SET Telefon= @Telefon, Efternavn = @Efternavn, Type = @Type, Fornavn = @Fornavn, 
+                        SET Telefon= @Telefon, AdresseID=@AdresseID Efternavn = @Efternavn, Type = @Type, Fornavn = @Fornavn, Mellemnavn=@Mellemnavn
                         WHERE PersonNummer = @PersonNummer";
 
             using (SqlCommand cmd = new SqlCommand(updateString, OpenConnection))
             {
                 // Get your parameters ready 
-                cmd.Parameters.AddWithValue("@Ansættelsedato", hv.Ansættelsedato.Date);
-                cmd.Parameters.AddWithValue("@Fagområde", hv.Fagområde);
-                cmd.Parameters.AddWithValue("@Fornavn", hv.Fornavn);
-                cmd.Parameters.AddWithValue("@HåndværkerId", hv.HID);
+                cmd.Parameters.AddWithValue("@Telefon", person.Telefon);
+                cmd.Parameters.AddWithValue("@Efternavn", person.Efternavn);
+                cmd.Parameters.AddWithValue("@Fornavn", person.Fornavn);
+                cmd.Parameters.AddWithValue("@PersonNummer", person.PersonNummer);
+                cmd.Parameters.AddWithValue("@Type", person.Type);
+                cmd.Parameters.AddWithValue("@Mellemnavn", person.Mellemnavn);
+                cmd.Parameters.AddWithValue("@AdresseID", person.Adresse);
 
                 var id = (int)cmd.ExecuteNonQuery();
-            }
-
-
-            try
-            {
-                // Open the connection
-                conn.Open();
-
-                // prepare command string
-                string updateString =
-                    @"UPDATE Person
-                        SET PersonNummer= @Data1, Efternavn = @Data2, Type = @Data3, Fornavn = @Data4
-                        WHERE PersonNummer = @Data5";
-                using (SqlCommand cmd = new SqlCommand(updateString, conn))
-                {
-                    // Get your parameters ready 
-                    cmd.Parameters.AddWithValue("@Data1", this.locPerson.Ansættelsedato.Date);
-                    //cmd.Parameters.Add(cmd.CreateParameter()).ParameterName = "@Data1";
-                    cmd.Parameters.Add(cmd.CreateParameter()).ParameterName = "@Data2";
-                    cmd.Parameters.Add(cmd.CreateParameter()).ParameterName = "@Data3";
-                    cmd.Parameters.Add(cmd.CreateParameter()).ParameterName = "@Data4";
-                    cmd.Parameters.Add(cmd.CreateParameter()).ParameterName = "@Data5";
-                    //cmd.Parameters["@Data1"].Value = this.locPerson.Ansættelsedato.Date;                  
-                    cmd.Parameters["@Data2"].Value = this.locPerson.Efternavn;
-                    cmd.Parameters["@Data3"].Value = this.locPerson.Fagområde;
-                    cmd.Parameters["@Data4"].Value = this.locPerson.Fornavn;
-                    cmd.Parameters["@Data5"].Value = this.locPerson.HID;
-
-
-
-                    var id = (int)cmd.ExecuteNonQuery(); //Returns the numbers of tuples/records affected
-                }
-
-
-            }
-            finally
-            {
-                // Close the connection
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-            }
+            }           
         }
 
-        public void DeleteCurrentPerson()
+        public void DeleteCurrentPerson(ref Person person)
         {
-            try
+            string deleteString = @"DELETE FROM Person WHERE (PersonNummer = @PersonNummer)";
+            using (SqlCommand cmd = new SqlCommand(deleteString, OpenConnection))
             {
-                // Open the connection
-                conn.Open();
+                cmd.Parameters.AddWithValue("@PersonNummer", person.PersonNummer);
 
-                // prepare command string
-                string deleteString =
-                   @"DELETE FROM Person
-                        WHERE (PersonNummer = @Data1)";
-                using (SqlCommand cmd = new SqlCommand(deleteString, conn))
-                {
-                    // Get your parameters ready 
-                    cmd.Parameters.Add(cmd.CreateParameter()).ParameterName = "@data1";
-
-                    cmd.Parameters["@Data1"].Value = this.locPerson.PersonNummer;
-
-                    var id = (int)cmd.ExecuteNonQuery(); //Returns thenumber of tuple/record affected
-                    locPerson = null;
-
-                }
-            }
-            finally
-            {
-                // Close the connection
-                if (conn != null)
-                {
-                    conn.Close();
-                }
+                var id = (int)cmd.ExecuteNonQuery();
+                person = null;
             }
 
         }
+
+        public List<Telefon> getPersons_Telefon(ref Person person)
+        {
+            string selectToolboxTelefonString = @"SELECT *
+                                                  FROM [har] 
+                                                  WHERE ([PersonNummer] = @PersonNummer)";
+            using (var cmd = new SqlCommand(selectToolboxTelefonString, OpenConnection))
+            {
+
+                SqlDataReader rdr = null;
+                cmd.Parameters.AddWithValue("@PersonNummer", person.PersonNummer);
+
+                rdr = cmd.ExecuteReader();
+                List<Telefon> telefons = new List<Telefon>();
+
+                Telefon tlf = null;
+                while (rdr.Read())
+                {
+                    tlf = new Telefon(); // 
+
+                    tlf.TelefonNummer = (string) rdr["TelefonNummerID"];
+                    tlf.Type= (string) rdr["Type"];
+                    telefons.Add(tlf);
+                }
+                return telefons;
+            }
+
+        }
+
+        public List<Adresse> getPersons_Adresse(ref Person person)
+        {
+            string selectToolboxAdresseString = @"SELECT *
+                                                  FROM [erPaa] 
+                                                  WHERE ([PersonNummer] = @PersonNummer)";
+            using (var cmd = new SqlCommand(selectToolboxAdresseString, OpenConnection))
+            {
+
+                SqlDataReader rdr = null;
+                cmd.Parameters.AddWithValue("@PersonNummer", person.PersonNummer);
+
+                rdr = cmd.ExecuteReader();
+                List<Adresse> adresses = new List<Adresse>();
+
+                Adresse adr = null;
+                while (rdr.Read())
+                {
+                    adr = new Adresse(); // 
+
+                    adr.Type = (string)rdr["Type"];
+                    //adr.AdresseID = (string)rdr["AdresseID"];
+                    adresses.Add(adr);
+                }
+                return adresses;
+            }
+
+        }
+
+
 
 
 
