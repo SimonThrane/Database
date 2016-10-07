09f5f6277c25b69c10e2a04a05a92e5ad0145fe3
SimonThrane <minijordan23-virum@hotmail.com>
2016-10-07 09:50:55 +0200

    Opdatering af PersonUtil.cs

--- Hand2in/TestApplication/Program.cs
+++ Hand2in/TestApplication/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccesTier;
 
 namespace TestApplication
 {
@@ -10,6 +11,9 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
+
+            var Test = new PersonDataUtil();
+
         }
     }
 }
