05909b102f61f3731a2486e65c797cefb1ee75d2
SimonThrane <minijordan23-virum@hotmail.com>
2016-12-13 08:16:29 +0100

    o

---
+++ Handin4/GDL/GDLssdtproject/dbo/Metadata_1.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[metadata]
+	AS SELECT [Sensorcharacteristics].calibrationCoeff, [Sensorcharacteristics].calibrationDate, [Sensorcharacteristics].[description], 
+	[Sensorcharacteristics].calibrationEquation, Appartmentcharacteristics.AppartmentId, Appartmentcharacteristics.Floor, Appartmentcharacteristics.Size
+	FROM [dbo].Sensorcharacteristics inner join Appartmentcharacteristics 
+	on (Appartmentcharacteristics.CharacteristicContainer_CharacteristicContainerId = 
+	Sensorcharacteristics.CharacteristicContainer_CharacteristicContainerId)
